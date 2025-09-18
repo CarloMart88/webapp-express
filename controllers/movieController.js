2b25fae2ec0807db58b826e8fe3cfe56da123363
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-18 18:06:47 +0200

    feat: fix const sql

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -8,7 +8,7 @@ const index = (req , res) =>{
   //creo la connessione 
   connection.query( sql , (err, result) =>{
     if(err) return res.status(500).json({error:"errore durante l'esecuzione della query"})
-      res.send(result)
+      res.json(result)
   })
 
 
@@ -18,13 +18,13 @@ const show = (req , res) =>{
 
   const { id } = req.params
   //creo la query
-  const sql = "SELECT * FROM movies WHERE movies_id = ? "
+  const sql = "SELECT * FROM movies WHERE id = ? "
 
   //creo la connessione 
   connection.query( sql ,[id], (err, result) =>{
     if(err) return res.status(500).json({error:"errore durante l'esecuzione della query"})
     if(result.length === 0 ) return res.status(404).json({ error: "Film non trovato" })
-      res.send(result[0])
+      res.json(result[0])
   })
 
 
