9b6459ebbbba47ac948a23bcbf1a0a53a72eba5e
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-18 17:00:17 +0200

    feat: index

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,1 +1,15 @@
 const connection = require("./data/db") // importo il database
+
+const index = (req , res) =>{
+
+  //creo la query
+  const sql = "SELECT * FROM movies "
+
+  //creo la connessione 
+  connection.query( sql , (err, result) =>{
+    if(err) return res.status(500).json({error:"errore durante l'esecuzione della query"})
+      res.json(result)
+  })
+
+
+}
